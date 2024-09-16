6e72cdee8e53b1e0bbddcf11eead304a90eda0c6
sasmithx <sasmithgit@gmail.com>
2024-09-16 12:02:23 +0530

    Reviews Section Completed

--- js/script.js
+++ js/script.js
@@ -3,3 +3,19 @@ let menu = document.querySelector('.menu-icon');
 menu.onclick = () => {
     menu.classList.toggle("move");
 };
+
+
+// Reviews Swipper
+
+var swiper = new Swiper(".reviews-content", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
