d82ec4f68b8c2c23ace34e159ada85083644f601
sasmithx <sasmithgit@gmail.com>
2024-09-20 08:09:24 +0530

    completed portfolio contact section with email functionality

--- js/script.js
+++ js/script.js
@@ -19,3 +19,48 @@ var swiper = new Swiper(".reviews-content", {
     clickable: true,
   },
 });
+
+// Email Js
+
+function validate() {
+  let name = document.querySelector('.name')
+  let email = document.querySelector(".email");
+  let msg = document.querySelector(".message");
+  let sendBtn = document.querySelector(".send-btn");
+
+  sendBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (name.value == "" || email.value == "'" || msg.value == ""){
+      emptyerror();
+    } else{
+      sendmail (name.value, email.value, msg,value);
+      success();
+    }
+  });
+}
+validate();
+
+function sendmail(name,email,msg){
+  emailjs.send("service_rpwcxk1", "template_4jmb3rd", {
+    to_name: name,
+    from_name: email,
+    message: msg,
+  });
+}
+
+function emptyerror() {
+  swal({
+    title: "Oh No....!",
+    text: "Fields cannot be empty!",
+    icon: "error",
+  });
+}
+
+function success() {
+  swal({
+    title: "Email sent successfully!",
+    text: "We try to reply in 24 hours",
+    icon: "success",
+  });
+}
+
