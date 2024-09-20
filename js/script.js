65c7f42d9fc372e3ee9f83db73098747e2380a78
sasmithx <sasmithgit@gmail.com>
2024-09-20 09:32:54 +0530

    completed portfolio footer section | Start responsive with media queries

--- js/script.js
+++ js/script.js
@@ -64,3 +64,18 @@ function success() {
   });
 }
 
+
+// Header Background Change On Scroll
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+  header.classList.toggle('header-active', window.scrollY > 0);
+});
+
+
+// Scroll Top
+let scrollTop = document.querySelector(".scroll-top");
+
+window.addEventListener("scroll", () => {
+  scrollTop.classList.toggle("scroll-active", window.scrollY >= 400);
+});
